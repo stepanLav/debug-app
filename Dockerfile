87bf1e585341a02d1747af995f28db3a8f19a61e
Stepan Lavrentev <lawrentievsv@gmail.com>
2023-11-21 13:41:54 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:14
+
+# Set the working directory in the container to /app
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install the application dependencies
+RUN npm install
+
+# Copy the rest of the application code to the working directory
+COPY . .
+
+# Expose port 4500 from the container to the host
+EXPOSE 4500
+
+# Define the command to run the application
+CMD [ "node", "app.js" ]
---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const { Pool } = require('pg');
+const app = express();
+const port = 4500;
+
+// Connect to the PostgreSQL database
+const pool = new Pool({
+  host: 'db',
+  user: 'postgres',
+  password: 'postgres',
+  database: 'postgres',
+  port: 5432,
+});
+
+// Create the coingecko_data table if it doesn't exist
+pool.query(`
+  CREATE TABLE IF NOT EXISTS coingecko_data (
+    id SERIAL PRIMARY KEY,
+    request_headers JSONB,
+    response_headers JSONB,
+    response_body JSONB,
+    response_code INTEGER,
+    user_uid TEXT
+  )
+`);
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/log', async (req, res) => {
+  const { request_headers, response_headers, response_body, response_code, user_uid } = req.body;
+
+  // Insert the data into the database
+  await pool.query(
+    'INSERT INTO coingecko_data (request_headers, response_headers, response_body, response_code, user_uid) VALUES ($1, $2, $3, $4, $5)',
+    [request_headers, response_headers, response_body, response_code, user_uid]
+  );
+
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
